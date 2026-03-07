04fbb5ed75cab4045511e4f0fc57761f9bee55b9
panospapako <p.d.papakostas@gmail.com>
2026-03-07 11:15:47 -0800

    feat: implement the semaphore for milestone4

--- Milestone4/Semaphore.py
+++ Milestone4/Semaphore.py
@@ -2,8 +2,9 @@
 # Semaphore.py
 # CS3070 
 #
+# Authors: Montes - Papakostas - Sami - Williams
 # created: Spring '16
-# updated: 13 Jan 2022
+# updated: 07 Mar 2026
 #
 
 '''This is the Semaphore implementaion class.'''
@@ -22,11 +23,23 @@ class Semaphore(object):
             This constructor will run once when the OS invokes it.
         '''
         
+        # Reference to the simulated OS kernel so we can use its services
+        # such as queues, locks, and waking sleeping processes.
         self.OS = simKernel
-        
-        #TODO: implement the rest of the constructor as required
 
+        # Semaphore counter. This represents how many processes
+        # are allowed to enter the critical section. In our lab the
+        # semaphore is initialized to 1.
+        self.count = n
 
+        # Queue used to store the names of processes that must wait
+        # because the semaphore counter became negative.
+        self.queue = self.OS.getQueue()
+
+        # Atomic lock used to guarantee that updates to the semaphore
+        # counter and queue happen atomically (preventing race conditions
+        # between concurrent wait() and signal() calls).
+        self.lock = self.OS.getAtomicLock()
 
 
 ##########################################
@@ -39,7 +52,33 @@ class Semaphore(object):
             - caller is the process asking "wait?"
               (you will need caller because this is a simulated system,
                a production OS has this info available as part of the PCB)'''
-        #TODO: implement 
+        
+        # Acquire the atomic lock to safely update the counter.
+        self.lock.acquire(caller)
+
+        # Decrement the semaphore counter
+        self.count -= 1
+
+        # If the counter becomes negative, this means the critical
+        # section is already occupied and the calling process must wait.
+        if self.count < 0:
+
+            # Place the process name in the semaphore waiting queue.
+            # This simulates the OS placing the process in a wait list.
+            self.queue.put(caller.getName())
+
+            # Release the lock before blocking so other processes
+            # can access the semaphore.
+            self.lock.release(caller)
+
+            # Put the calling process to sleep until another process
+            # performs a signal() and wakes it.
+            caller.sleep()
+        else:
+
+            # If the counter is still >= 0, the process can continue
+            # into the critical section, so we release the lock.
+            self.lock.release(caller)
 
 
 
@@ -49,7 +88,27 @@ class Semaphore(object):
             - caller is the process providing the "signal"
               (you will need caller because this is a simulated system,
                a production OS has this info available as part of the PCB)'''
-        #TODO: implement 
+        
+        # Acquire the atomic lock to safely update the counter.
+        self.lock.acquire(caller)
+
+        # Increment the semaphore counter to indicate that the
+        # critical section has been released.
+        self.count += 1
+
+        # If the counter is <= 0, there are processes waiting
+        # in the semaphore queue.
+        if self.count <= 0:
+
+            # Remove one waiting process from the queue.
+            process_name = self.queue.get()
+
+            # Wake that process so it can continue execution.
+            self.OS.wake(process_name)
+
+        # Release the lock so other processes can access the semaphore.
+        self.lock.release(caller)
+
 
 
 
